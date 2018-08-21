ed26ba244c2829f1dfda2beb9fc9c71fa694ca1f
Rodrigo Arriaza <arr15334@uvg.edu.gt>
2018-08-20 19:18:00 -0600

    error

---
+++ app.js
@@ -0,0 +1,95 @@
+var express = require('express')
+var app = express()
+var server = require('http').Server(app);
+var io = require('socket.io')(server)
+var axios = require('axios')
+var qs = require('qs')
+let totalMessages = 0
+
+server.listen('3000')
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+
+
+io.on('connection', (socket) => {
+  // console.log('User connected')
+  // socket.username = 'Anonymous'
+  // socket.on('change_username', (data) => {
+  //   socket.username = data.username
+  // })
+  // console.log(socket);
+  socket.on('new_message', (data) => {
+    const msg = data.msg
+    const nick = data.nickname
+    const studentId = data.studentId
+    console.log('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
+    axios.post('http://34.210.35.174:7000',
+      qs.stringify({
+        'student_id': studentId,
+        'nick': nick,
+        'text': msg
+      }),
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      }
+  )
+    .then(() => {
+      io.sockets.emit('new_message', {
+        'student_id': studentId,
+        'nick': nick,
+        'text': msg
+      })
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  })
+})
+
+function checkChat () {
+  return axios.get('http://34.210.35.174:7000')
+    .then(response => {
+      return response.data
+    })
+    // .then((response) => response.json())
+    .then(response => {
+      let chatHistory = [];
+			for (const resp of response) {
+				chatHistory.push({
+					studentId: resp['student_id'],
+					text: resp['text'],
+					nickname: resp.nick
+				})
+			}
+			return chatHistory;
+    })
+    .then((chatHistory) => {
+			for (let i = 0; i < chatHistory.length; i++) {
+				if ((i+ 1) > totalMessages) {
+          // console.log(chatHistory[i])
+          const chatObj = chatHistory[i]
+          console.log(chatObj);
+          io.emit('new_message', {
+            'student_id': chatObj.studentId,
+            text: chatObj.text,
+            nick: chatObj.nickname
+          })
+				}
+			}
+			return chatHistory.length
+		})
+		.then((chatSize) => {
+			totalMessages = chatSize
+		})
+}
+
+// checkChat()
+setInterval(checkChat, 7000)
---
+++ public/chat.js
@@ -0,0 +1,42 @@
+
+
+
+$(function(){
+  console.log('fasdf');
+  var socket = io.connect('http://localhost:3000')
+  socket.on('news', function(data) {
+    console.log(data);
+  })
+  $('form').submit(function () {
+    console.log('asdf');
+    console.log(socket);
+    socket.emit('new_message', {studentId: '15335', nickname: 'Sidd', msg: $('#m').val()});
+    $('#m').val('');
+    return false;
+  });
+  socket.on('new_message', function(msg){
+    console.log(msg);
+    // $('#messages').append($('<li>').text(msg.text));
+
+    // TODO revisar si studentId = 15334
+    // TODO ver como agregar clase a elemento dinamicamente
+    const theChat = document.getElementById('theChat')
+    const li = document.createElement('li');
+    const strong = document.createElement('strong');
+    const strongText = document.createTextNode(msg.nick);
+    const liText = document.createTextNode('('+msg['student_id']+'): '+msg.text);
+
+    if (msg['student_id'] == '15334') {
+      li.className = 'myMessages'
+    }
+    /*
+    <li><strong>nickname</strong>(studentId): text </li>
+    */
+    strong.appendChild(strongText);
+    li.appendChild(strong);
+    li.appendChild(liText);
+
+    theChat.appendChild(li);
+    // window.scrollTo(0, document.body.scrollHeight);
+  });
+})
